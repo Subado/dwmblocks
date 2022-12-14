5cebe1b35c100fb6ba37aa93157492e92a9756c5
Subado <redcatgamer1980@gmail.com>
2022-12-14 20:40:39 +0400

    Setup blocks for bar

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{" ", "xbps-install -un | awk '{print $2}' | wc -l",	300,		0},
+	{"", "xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print \"[RU]\"; else print \"[EN]\"; }'", 1, 0},
+	{" ", "date '+%b %d (%a)'",					5,		0},
+	{" ", "date '+%R'",					5,		1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ┇ ";
 static unsigned int delimLen = 5;
