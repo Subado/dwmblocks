92e21166d00ea8b9acd3e9b92f6ad2bd72f711de
Subado <redcatgamer1980@gmail.com>
2022-12-17 12:07:16 +0400

    Add block with the main goal

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "xbps-install -un | awk '{print $2}' | wc -l",	300,		0},
-	{"", "xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print \"[RU]\"; else print \"[EN]\"; }'", 1, 0},
-	{" ", "date '+%b %d (%a)'",					5,		0},
-	{" ", "date '+%R'",					5,		1},
+	{"", "echo 'fuck procrastination'", 0, 0},
+	{" ", "xbps-install -un | awk '{print $2}' | wc -l", 300, 0},
+	{"", "xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print \"[RU]\"; else print \"[US]\"; }'", 1, 0},
+	{" ", "date '+%b %d (%a)'", 5, 0},
+	{" ", "date '+%R'", 5, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
