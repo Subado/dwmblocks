dc5a46f6a6bfd4f0bb5e3632abff451c120c9c13
Subado <redcatgamer1980@gmail.com>
2022-12-22 03:49:36 +0400

    Edit block with the main goal

--- blocks.def.h
+++ blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "curl wttr.in/Sarapul?format=\"%c%t+%m\"", 3600, 0},
-	{"", "echo 'нахуй прокрастинацию🖕'", 0, 0},
+	{"", "echo 'Что ты делаешь?🤨'", 0, 0},
 	{"󰇚 ", "xbps-install -un | awk '{print $2}' | wc -l", 300, 0},
 	{"", "xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print \"[RU]\"; else print \"[US]\"; }'", 1, 0},
 	{"󰸗 ", "date '+%b %d (%a)'", 5, 0},
