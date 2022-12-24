76346c7ae82b64e889d9f628e3fff1d5d72988b4
Subado <redcatgamer1980@gmail.com>
2022-12-24 11:45:50 +0400

    Add slient option to curl

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "curl wttr.in/Sarapul?format=\"%c%t+%m\"", 3600, 0},
+	{"", "curl -s wttr.in/Sarapul?format=\"%c%t+%m\"", 3600, 0},
 	{"", "echo 'Нахуй лишнюю информацию💀'", 0, 0},
 	{"󰇚 ", "xbps-install -un | awk '{print $2}' | wc -l", 300, 0},
 	{"", "xset -q|grep LED| awk '{ if (substr ($10,5,1) == 1) print \"[RU]\"; else print \"[US]\"; }'", 1, 0},
